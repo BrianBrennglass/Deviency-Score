dcba75dc941331da0369e9e9fb65227fdb7399cb
Brian Brennglass <Bbrenng1@binghamton.edu>
2021-10-17 00:06:02 -0400

    backing up

--- get_user_data.py
+++ get_user_data.py
@@ -1,4 +1,5 @@
 import praw
+from collections import Counter
 
 reddit = praw.Reddit(
     client_id="RyhoH9FE3QNpXIe5stCQ7A",
@@ -6,15 +7,27 @@ reddit = praw.Reddit(
     user_agent="appTester123"
 )
 
-submission = reddit.submission(url="""https://www.reddit.com/r/Python/comments/1g22nr/praw_user_agent_im_a_noob/""")
+#submission = reddit.submission(url="""https://www.reddit.com/r/Python/comments/1g22nr/praw_user_agent_im_a_noob/""")
 
 
 #sub = redditor.submissions.new(limit=100)
 #comments = reddit.comments.new(limit=50)
-all = reddit.redditor("lothdran").new(limit=100)
-l = 0
-for i in all:
-	l+=1
-	print(i.subreddit, i.permalink, i.over_18)
+#all = reddit.redditor("lothdran").new(limit=100)
 
-print('\n', all)
+#subs = [sub.subreddit for sub in all]
+#subCount = Counter(subs)
+#print(subCount)
+
+user = reddit.subreddit('random').new(limit=1)
+for i in user:
+	print(i.subreddit, i.author)
+
+
+#user_engagements = reddit.redditor(user).new(limit=100)
+#for engagement in user_engagements:
+#	print(engagement.subreddit)
+	
+#for i in subreddit:
+#	print(i.author)
+#help(subreddit)
+#print(subreddit)
--- scrape_nsfw/convert_ndjson_2_json.py
+++ scrape_nsfw/convert_ndjson_2_json.py
@@ -3,14 +3,16 @@ import json
 # the content here could be read from a file instead
 
 result = []
-with open('./nswf_subs.ndjson', 'r') as ndjson, open('./nsfw_subs.json', 'w') as json_file:
+with open('./nsfw_subs.ndjson', 'r') as ndjson_file, open('./nsfw_subs.json', 'w') as json_file:
+	ndjson = ndjson_file.read()
+	result = []
 	for line in ndjson.splitlines():
 		print(line)
 		if not line.strip():
 			continue
 		next_line = json.loads(line)
 		result.append(next_line)
-	json_file.write(result)
+	json_file.write(json.dumps(result))
 
 
 
